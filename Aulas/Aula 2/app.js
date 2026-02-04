b8ab78bc0ff3be1e1d26b2ee9e6e6be8bc25e65c
lucasaleduarte <lucasaleduarte0@gmail.com>
2026-02-04 11:06:06 -0300

    Aula 2

--- Aulas/Aula 2/app.js
+++ Aulas/Aula 2/app.js
@@ -78,16 +78,47 @@ entradaDeDados.question('Digite o nome do aluno: ', function(nome){
                         NÃO -> NOT  -> !
 
                     */
+                    /*
+                        Conversões de tipos de dados
+                            parseInt() -> Permite converter uma String para número INTEIRO
+                            parseFloat() -> Permite converter uma String para número Decimal
+                            Number() -> Permite converter uma String para NUMERO (INTEIRO OU DECIMAL)
+                            String() -> Permite converter um conteúdo para STRING
+                            Boolean() -> Permite converter um conteúdo para BOOLEANO
+                            typeof() -> Permite verificar o tipo de dados de uma variável
+                            toFixed() -> Permite fixar a qtde de casas decimais 
+                    */ 
                     
                     //Validação de entrada vazia
                     if(nomeAluno == '' || nota1 == '' || nota2 == '' || nota3 == '' ||  nota4 == ''){
-                        console.log('ERRO: É obrigatório o preenchimento de todos os dados!!!')                        
+                        console.log('ERRO: É obrigatório o preenchimento de todos os dados!!!')
+                    //Validação de numeros entre 0 a 100                        
                     }else if(nota1 < 0 || nota2 < 0 || nota3 < 0 || nota4 < 0 
                         || nota1 > 100 || nota2 > 100 || nota3 > 100 || nota4 > 100){
-                        console.log('Erro')
+                        console.log('Erro: Somente é permitido a entrada de valores ente 0 e 100.')
+                    //Validação para a entrada de letras nas notas
+                    //isNaN() -> Permite validar se o conteudo da 
+                            //variavel tem algum caracter ao invés de número
+                    //Não se usa a validação de uma função já boolean EX: == true; Não se usa
+                    }else if(isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)){
+                        console.log('Erro: Não é possivel calcular a média com a entrada de letras nas notas do aluno!!!')
+                    }else{
+                            let statusAluno
+                            //Calculo da Média
+                            let media = (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4))/4
 
-                    }
+                            //Validação do Satus do Aluno
+                            if(media <= 50){
+                                statusAluno = 'REPROVADO'
+                            }else if(media > 50 && media < 70){
+                                statusAluno = 'RECUPERÇÃO'
+                            }else{
+                                statusAluno = 'APROVADO'
+                            }
 
+                            //Saída do boletim do aluno
+                            console.log(`Aluno(a):${nomeAluno} \nMédia Final: ${media.toFixed(2)} \nStatus do Aluno: ${statusAluno}`)
+                    }//Fecha else statusAluno
 
                 })//Fecha a nota 4
             })//Fecha a nota 3
